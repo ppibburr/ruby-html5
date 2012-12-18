47a35f9c106126b9c41df5dc7d5e7601e78e46ee
ppibburr <ppibburr@laptop.(none)>
2012-12-18 14:27:52 -0500

    ...,

--- test/load_test.rb
+++ test/load_test.rb
@@ -7,3 +7,21 @@ describe "Upon load" do
     GirFFISugar
   end  
 end
+
+describe Gtk::Container do
+  describe "#children" do
+    it "should return array" do
+      v = Gtk::VBox.new
+      v.add Gtk::Label.new
+      assert_instance_of Array,v.children    
+    end
+  end
+end
+
+describe Gtk do
+  describe "init" do
+    it "should accept 0 args" do
+      Gtk.init 
+    end
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,11 @@
 require 'rubygems'
 require 'minitest/spec'
 require 'minitest/autorun'
-
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/test/"
+end
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'html5'
+
